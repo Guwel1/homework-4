2a5dfc8fd8d38cd7c7b1c039ee70fb64093445a7
Guwel <guwel@yandex.ru>
2022-12-08 01:42:05 +0500

    add homework

---
+++ homework/Task 25/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+Console.Clear();
+Console.WriteLine("Введите число A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int number = a;
+if (b==0)
+number=1;
+for (int i = 2; i <= b; i ++ )
+        number = number*a;
+Console.Write($"Число А, возведеннео в степень В: {number}");
---
+++ homework/Task 29/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов, запрашивает данные(элементы массива) у пользователя и выводит их на экран.
+Console.Clear();
+int n = 8;
+int[] array = new int[n];
+for (int i = 0; i < array.Length; i++)
+{
+Console.Write($"Введите {i + 1} элемент массива: ");
+array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine($"[{string.Join(", ", array)}]");
